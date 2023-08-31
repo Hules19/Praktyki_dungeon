ea46d22cc47218cc13498820754cfca152bc749d
Aiden <unnamedrealhero@gmail.com>
2023-08-31 17:34:53 +0200

    Animation and movement

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -4,40 +4,110 @@ using UnityEngine;
 
 public class PlayerActions : MonoBehaviour
 {
-   public Ray ray;
-   private float maxDistance = 2;
-   public LayerMask layersToHit;
-   public bool canInteract;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private Vector3 moveDirection;
 
-	public float health;
-    public float damage;
+    [SerializeField] private float health;
+    [SerializeField] private float damage;
+    [SerializeField] private bool canInteract;
 
-   void Start()
-   {
-      ray = new Ray(transform.position, transform.forward);
-      CheckForColliders();
-   }
+    private CharacterController controller;
+    private Animator mAnimator;
+    //private Collider collider;
+    
+    void Start()
+    {
+        mAnimator = GetComponentInChildren<Animator>();
+        controller = GetComponent<CharacterController>();
+        //collider = GetComponentInChildren<CapsuleCollider>();
+    }
 
-   void CheckForColliders()
-   {
-      if (Physics.Raycast(ray, out RaycastHit hit, maxDistance, layersToHit))
-      {
-         Debug.Log(hit.collider.gameObject.name + "was detected");
-         canInteract = true;
-      }
-   }
+    void Update()
+    {
+        Move();
+        if (Input.GetMouseButton(0))
+        {
+            StartCoroutine(AttackAnimation());
+            Attack();
+        }
+    }
+    
+    //ATTACK
+    private IEnumerator AttackAnimation()
+    {
+        mAnimator.SetLayerWeight(mAnimator.GetLayerIndex("Attack Layer"), 1);
+        mAnimator.SetTrigger("TrAttack");
+
+        yield return new WaitForSeconds(0.9f);
+        mAnimator.SetLayerWeight(mAnimator.GetLayerIndex("Attack Layer"), 0);
+    }
+
+    private void Attack()
+    {
+        
+    }
+    
+    //MOVEMENT
+    private void Move()
+    {
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(0, 0, moveZ);
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+        {
+            Walk();
+        }
+        else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+        {
+            Run();
+        }
+        if (moveDirection  == Vector3.zero)
+        {
+            Idle();
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void Idle()
+    {
+        mAnimator.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+        mAnimator.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+    }
+
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+        mAnimator.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
+    }
+    
+    //INVOKING INTERACTION
+
+    void OnCollisionEnter(Collision collision)
+    {
+        canInteract = true;
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Interactables"))
+        {
+           if (canInteract = true && Input.GetKeyDown("E"))
+                   {
+                       Debug.Log("Interaction completed");
+                       Interaction();
+                   } 
+        }
+        
+    }
    
-   void Interaction()
-   {
-      if (canInteract = true && Input.GetKeyDown("E"))
-         {
-            //interact;
-			//GetComponent.
-			}
-
-	}
-
- 
+    void Interaction()
+    {
+        //.
+    }
 }
-
-  
